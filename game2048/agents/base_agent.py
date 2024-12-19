693d71c91fec0dca5e3905b403d7381028754099
kon-foo <25391223+kon-foo@users.noreply.github.com>
2024-12-19 11:06:46 +0100

    add save methods for metrics and recording

--- game2048/agents/base_agent.py
+++ game2048/agents/base_agent.py
@@ -1,3 +1,6 @@
+import csv
+import os
+from pathlib import Path
 from abc import ABC, abstractmethod
 
 from game2048 import Game2048, Move, GameRecord
@@ -13,13 +16,15 @@ class Agent(ABC):
     Base interface for all agents.
     """
 
-    def __init__(self, save_base_path: str = "recordings", visualizer: Visualizer = None):
+    def __init__(self, save_base_path: str = "results", visualizer: Visualizer = None):
         """
         Initialize the agent.
         """
 
         self.game: Game2048 = None
         self.save_base_path = save_base_path
+        self.save_recordings_path = save_base_path + "/recordings"
+        self.save_metrics_path = save_base_path + "/metrics"
         self.move_metadata: dict[int, dict[str, any]] = {}
         self.agent_metadata: dict[str, any] = {}
         self.metrics = AgentMetrics()
@@ -35,13 +40,6 @@ class Agent(ABC):
         """
         return self.__class__.__name__
     
-
-    def _save_recording(self):
-        """
-        Save the recording of the game.
-        """
-        self.game.save_recording(self.name)
-
 
     def add_move_metadata(self, move_idx: int = None, **kwargs):  
         """
@@ -60,6 +58,34 @@ class Agent(ABC):
         """
         if kwargs:
             self.agent_metadata.update(kwargs)
+
+    def save_metrics(self):
+        """
+        Save the metrics of the agent.
+        """
+        path = Path(self.save_metrics_path)
+        path.mkdir(parents=True, exist_ok=True)
+
+        filename = f"{self.name}_metrics.csv"
+        file_path = path / filename
+        data = self.metrics.to_row()
+        if os.path.exists(file_path):
+            with open(file_path, 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(data)
+        else:
+            header = self.metrics.to_header()
+            with open(file_path, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(header)
+                writer.writerow(data)
+
+
+    def save_recording(self, recording: GameRecord):
+        """
+        Saves a recording of a game.
+        """
+        recording.save(base_path=self.save_recordings_path)
 
 
     def play(self, seed: int = None, save_recording: bool = False, max_rounds: int = 0) -> GameRecord:
@@ -96,8 +122,8 @@ class Agent(ABC):
             print(f"Game Over! Final score: {self.game.state.score}")
 
         if save_recording:
-            recording.save(base_path=self.save_base_path)
-
+            self.save_recording(recording)
+        
         return recording
 
 
